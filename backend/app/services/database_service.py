14fac4de743700077cca0d15c9546d55636816fd
sobachevskiy <dsobachevsky@gmail.com>
2026-03-01 12:27:04 +0500

    docker + db

--- backend/app/services/database_service.py
+++ backend/app/services/database_service.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost/db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
@@ -10,3 +11,7 @@ SessionLocal = sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
+
+async def get_session() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
